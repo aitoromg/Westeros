34ebd966f3afabe2e1e85599be8f876cf426b531
Alexandre Freire <xandrefreire@gmail.com>
2018-09-18 20:22:52 +0200

    HouseListVC

---
+++ Westeros/Westeros/Controllers/WikiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WikiViewController.swift
+//  Westeros
+//
+//  Created by Alexandre Freire on 17/09/2018.
+//  Copyright © 2018 Alexandre Freire. All rights reserved.
+//
+
+import UIKit
+
+class WikiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
